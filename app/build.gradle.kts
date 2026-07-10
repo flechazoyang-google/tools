ca772da67d1552a47c55463ab6db015fba6ace0f
“flechazoyang-google” <flechazoyang@gmail.com>
2026-07-10 16:02:45 +0800

    release: v1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.toolbox"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.3.4"
+        versionCode = 13
+        versionName = "1.4.0"
     }
 
     buildTypes {
